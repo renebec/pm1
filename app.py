4fd141dcc802b7a4d2259172ea528d31a5c87bdf
renebec <39104197+renebec@users.noreply.github.com>
2025-08-12 23:48:01 +0000

    added json space POSIX friendlines

--- app.py
+++ app.py
@@ -426,7 +426,7 @@ def list_temas():
     # Insert blank lines between objects
     spaced = pretty.replace("},", "},\n")
     # Optionally ensure a newline at end of file for POSIX friendliness
-    #spaced = spaced + "\n"
+    spaced = spaced + "\n"
     return Response(spaced, mimetype='application/json')
 
 #if __name__ == "__main__":
