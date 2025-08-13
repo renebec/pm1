46cc9904638253ca2de44bda449202987fd7fb29
renebec <39104197+renebec@users.noreply.github.com>
2025-08-13 00:39:50 +0000

    reactivate app.config jason

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from gevent import monkey; monkey.patch_all()
 from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
-#app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 TEMAS = [
   {
